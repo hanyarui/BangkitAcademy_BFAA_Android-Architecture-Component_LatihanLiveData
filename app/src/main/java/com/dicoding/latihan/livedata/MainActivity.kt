471785a8d96344aff5ae611bf65a8baa5b08dba2
ruinzyu <dharuaulia1@gmail.com>
2024-04-24 23:00:39 +0700

    first commit

---
+++ app/src/main/java/com/dicoding/latihan/livedata/MainActivity.kt
@@ -0,0 +1,34 @@
+package com.dicoding.latihan.livedata
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import com.dicoding.latihan.livedata.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var liveDataTimerViewModel: MainViewModel
+    private lateinit var activityMainBinding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        activityMainBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(activityMainBinding.root)
+
+        liveDataTimerViewModel = ViewModelProvider(this)[MainViewModel::class.java]
+        subscribe()
+        }
+
+    private fun subscribe() {
+        val elapsedTimeObserver = Observer<Long?> { along ->
+            val newText = this@MainActivity.resources.getString(R.string.seconds, along)
+            activityMainBinding.timerTextview.text = newText
+        }
+
+        liveDataTimerViewModel.getElapsedTime().observe(this, elapsedTimeObserver)
+    }
+}
